8e71d5e81da51d6c98c39b45744cd2f5f497fa45
Oyesh Mann Singh <oyesh.mann.singh@hp.com>
2022-12-27 17:26:40 -0800

    Increased page size to 100

--- weather/settings.py
+++ weather/settings.py
@@ -80,7 +80,7 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 100,
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
